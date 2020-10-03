3b7ac2c55e6c37fd1984265a6bfe38d8e794ede5
C. Richard Matson <pgoldfarm@yahoo.com>
2020-10-03 00:21:54 -0500

    good night!

---
+++ HelloWorld.py
@@ -0,0 +1,51 @@
+# while True:
+#     try:
+#         x=int(input("Put a number:"))
+#         print("Yup!, that is a number")
+#         break
+#     except ValueError:
+#         print("That is not a number")
+
+
+
+# list_number = [1,2,3,4,5,6,7,8]
+
+# for list_number in list_number:
+#     print("List numbers :", list_number)  # List numbers : 1
+#                                         # List numbers : 2
+#                                         # List numbers : 3
+#                                         # List numbers : 4
+#                                         # List numbers : 5
+#                                         # List numbers : 6
+#                                         # List numbers : 7
+#                                         # List numbers : 8
+
+
+# fruits = 'Banana'
+
+# remove = 'n'
+
+# result = ''
+
+# for new_name in fruits:
+#     if new_name.lower() not in remove:
+#         result += new_name
+
+# print(result)  # Baaa
+
+
+
+
+for a in range(1,5):
+    for b in range(0,a):
+        print(a)  # 1
+                    # 2
+                    # 2
+                    # 3
+                    # 3
+                    # 3
+                    # 4
+                    # 4
+                    # 4
+                    # 4
+
